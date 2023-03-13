52078d1a395b980f076e2c8ab8c955cae8ea7ae0
Ashley Liu <ashlel13@circinus-43.ics.uci.edu>
2023-03-13 13:30:36 -0700

    add hw9 files

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-        System.out.println("a new line");
-        Stuff s = new Stuff(3);
-        System.out.println(s.getStuffData());
-    }
-}
